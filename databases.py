8244b107e935d9342e0cd6955419a4ec34fe2168
ThaboChauke <chaukethabo16@gmail.com>
2024-09-18 21:45:39 +0200

    add tables to database

--- databases.py
+++ databases.py
@@ -1,13 +1,36 @@
 import sqlite3
 
 
-def create_table(connection, cursor):
+def create_table(cursor):
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS normal_mode (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    score INTEGER
+)
+""")
+    
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS hard_mode (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    score INTEGER
+)
+""")
+    
+def update_score():
     pass
+    
 
 
 def main():
     connection = sqlite3.connect('scoreboard.db')
     cursor = connection.cursor()
+
+    create_table(cursor)
+
+    connection.commit()
+    connection.close()
 
 
 if __name__ == '__main__':
