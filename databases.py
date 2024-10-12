e1caae0d62394c00e55af28cb30277967f44fc2c
ThaboChauke <chaukethabo16@gmail.com>
2024-10-12 21:05:06 +0200

    add functions for viewing and updating scores

--- databases.py
+++ databases.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def create_table(cursor):
+def create_tables(cursor):
     cursor.execute("""
 CREATE TABLE IF NOT EXISTS normal_mode (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -18,20 +18,39 @@ CREATE TABLE IF NOT EXISTS hard_mode (
 )
 """)
     
+
+def view(cursor, mode):
+    if mode == "normal":
+        mode_query = "normal_mode"
+    elif mode == "hard":
+        mode_query = "hard_mode"
     
-def update_score():
-    pass
+    cursor.execute(f"SELECT * FROM {mode_query} LIMIT 10")
+    return cursor.fetchall()
+  
     
+def update_score(cursor, name, score, mode):
+    if mode == "normal":
+        table = "normal_mode"
+    elif mode == "hard":
+        table = "hard_mode"
+
+    cursor.execute(f"SELECT * FROM {table} WHERE name = ?", (name,))
+    result = cursor.fetchone()
+
+    if result:
+        cursor.execute(f"UPDATE {table} SET score = ? WHERE name = ?", (score, name))
+    else:
+        cursor.execute(f"INSERT INTO {table} (name, score) VALUES (?, ?)", (name, score))
+
 
 
 def main():
-    connection = sqlite3.connect('scoreboard.db')
-    cursor = connection.cursor()
+    with sqlite3.connect('scoreboard.db') as connection:
+        cursor = connection.cursor()
+        create_tables(cursor)
+        connection.commit()
 
-    create_table(cursor)
-
-    connection.commit()
-    connection.close()
 
 
 if __name__ == '__main__':
